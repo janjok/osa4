c232b796b79427ba4c4999ce31b212304afc4257
janjok <118051663+janjok@users.noreply.github.com>
2024-08-22 13:30:52 +0300

    4.5 apufunktioita ja yksikkötestejä, step3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,36 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    if(blogs.length === 0){ return 0}
+    
+    const total = blogs.reduce((acc, cur) => acc + cur.likes, 0)
+
+    return total
+}
+
+const favoriteBlog = (blogs) => {
+    let mostLikes = 0
+    const fav = {
+        title: "",
+        author: "",
+        likes: 0
+    }
+
+    blogs.forEach(x => {
+        if(x.likes > mostLikes){
+            fav.title = x.title
+            fav.author = x.author
+            fav.likes = x.likes
+            mostLikes = x.likes
+        }
+    });
+    
+    return fav
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes,
+    favoriteBlog
 }
